eb73a1e39a596e4349894df1faa784bc3855c654
Greig <40172289@live.napier.ac.uk>
2017-01-21 15:22:45 +0000

    Re-structure turret scripts

---
+++ Assets/Scripts/Turrets/ProjectileTurret.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileTurret : Turret {
+    public GameObject projectile;
+    public Transform shotOrigin;
+
+    protected override void AquireTarget()
+    {
+        base.AquireTarget();
+
+        float timeToHit = Vector3.Distance(target.transform.position, transform.position) / projectile.GetComponent<Shot>().speed;
+
+        Vector3 targetVelocity = target.GetComponent<Rigidbody2D>().velocity;
+
+        Vector3 aimPoint = target.transform.position + (targetVelocity * timeToHit);
+
+        Vector3 rotDir = (aimPoint - transform.position);
+        float angle = Mathf.Atan2(rotDir.y, rotDir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    protected override void Fire()
+    {
+        Instantiate(projectile, shotOrigin.transform.position, this.transform.rotation);
+        base.Fire();
+    }
+}
---
+++ Assets/Scripts/Turrets/Turret.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Turret : MonoBehaviour {
+    public float range;
+    public float fireRate;
+
+    protected List<Collider2D> HostileList = new List<Collider2D>();
+    protected GameObject target;
+    protected float nextFire;
+
+    void Start()
+    {
+        gameObject.GetComponent<CircleCollider2D>().radius = range;
+    }
+
+    void FixedUpdate()
+    {
+        if (HostileList.Count != 0)
+        {
+            AquireTarget();
+
+            //Make it follow a target
+            if (Time.time >= nextFire)
+            {
+                Fire();
+            }
+        }
+    }
+
+    protected virtual void AquireTarget()
+    {
+        Collider2D selected = null;
+        float minDist = float.PositiveInfinity;
+        foreach (Collider2D enemy in HostileList)
+        {
+            if (enemy == null)
+            {
+                HostileList.Remove(enemy);
+            }
+            if (enemy.GetComponentInParent<WalkerAI>().distToEnd < minDist)
+            {
+                selected = enemy;
+                minDist = enemy.GetComponentInParent<WalkerAI>().distToEnd;
+            }
+        }
+        target = selected.gameObject;
+    }
+
+    protected virtual void Fire()
+    {
+        nextFire = Time.time + fireRate;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Arse");
+        if (!HostileList.Contains(other) && other.tag == "Enemy")
+        {
+            HostileList.Add(other);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        print("De-Arse");
+        if (HostileList.Contains(other) && other.tag == "Enemy")
+        {
+            HostileList.Remove(other);
+        }
+    }
+}
