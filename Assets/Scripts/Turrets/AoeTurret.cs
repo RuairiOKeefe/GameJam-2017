02cacc78d28ffcb98ef6b0d220d6f17e267bb43c
Aaron <aarongreig01@gmail.com>
2017-01-21 16:56:48 +0000

    Add AoeTurret script, make Mover.Move() public and remove debug outputs in Turret

---
+++ Assets/Scripts/Turrets/AoeTurret.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoeTurret : Turret {
+    public float Damage;
+    public float KnockbackStrength;
+
+    // Use this for initialization
+    protected override void Fire()
+    {
+        foreach (Collider2D enemy in HostileList)
+        {
+            enemy.gameObject.GetComponent<Health>().TakeDamage(Damage);
+
+            Vector3 knockBackVec = (enemy.transform.position - transform.position).normalized * KnockbackStrength;
+
+            // TODO: decide how we will actually be handling this
+            enemy.gameObject.GetComponent<Mover>().Move(knockBackVec.x, knockBackVec.y);
+        }
+
+        base.Fire();
+    }
+}
