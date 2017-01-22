a1ee14dd7029afbd46c6eac954776a0ecdc0c411
Aaron <aarongreig01@gmail.com>
2017-01-22 01:39:45 +0000

    Implement aoe projectile turret/shot scripts and waterbomb prefabs

---
+++ Assets/Scripts/Turrets/AoeProjectileTurret.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AoeProjectileTurret : ProjectileTurret {
+    protected override void AquireTarget()
+    {
+        base.AquireTarget();
+        Vector3 rotDir = target.transform.position - transform.position;
+        float angle = Mathf.Atan2(rotDir.y, rotDir.x) * Mathf.Rad2Deg;
+        this.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+    }
+
+    protected override void Fire()
+    {
+        projectile.GetComponent<AoeShot>().targetPos = target.transform.position;
+        base.Fire();
+    }
+}
